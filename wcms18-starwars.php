1bfaf7fb288704fa273a4da1e4a24a7ebf39890b
Johan Nordström <johan@digitalvillage.se>
2019-05-22 13:13:14 +0200

    Initial commit. Working version of getting all StarWars films.

---
+++ wcms18-starwars.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WCMS18 StarWars Widget
+ * Plugin URI:  https://thehiveresistance.com/wcms18-starwars
+ * Description: This plugin for displaying some StarWars trivia.
+ * Version:     0.1
+ * Author:      Johan Nordström
+ * Author URI:  https://thehiveresistance.com
+ * License:     WTFPL
+ * License URI: http://www.wtfpl.net/
+ * Text Domain: wcms18-starwars
+ * Domain Path: /languages
+ */
+
+require("class.StarWarsWidget.php");
+
+function wsw_widgets_init() {
+	register_widget('StarWarsWidget');
+}
+add_action('widgets_init', 'wsw_widgets_init');
