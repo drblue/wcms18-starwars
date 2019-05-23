de999353d29cc1fb34f46c7b49dcd641187eaff0
Johan Nordström <johan@digitalvillage.se>
2019-05-23 15:13:34 +0200

    OMG so good version with abstraction of retrieving multiple pages of items.

--- swapi.php
+++ swapi.php
@@ -12,65 +12,44 @@ function swapi_get_url($url) {
 	return json_decode(wp_remote_retrieve_body($response));
 }
 
-function swapi_get($endpoint) {
-	$items = [];
-	$url = "https://swapi.co/api/{$endpoint}/";
-	while ($url) {
-		$data = swapi_get_url($url);
-		if (!$data) {
-			return false;
-		}
-		$items = array_merge($items, $data->results);
+function swapi_get($endpoint, $expiry = 3600) {
+	$transient_key = "swapi_get_{$endpoint}";
+	$items = get_transient($transient_key);
 
-		$url = $data->next; // "https://swapi.co/" | null
+	if ($items) {
+		return $items;
+
+	} else {
+		$items = [];
+		$url = "https://swapi.co/api/{$endpoint}/";
+		while ($url) {
+			$data = swapi_get_url($url);
+			if (!$data) {
+				return false;
+			}
+			$items = array_merge($items, $data->results);
+
+			$url = $data->next; // "https://swapi.co/" | null
+		}
+
+		// save for future use
+		set_transient($transient_key, $items, $expiry);
+
+		// return items
+		return $items;
 	}
-	return $items;
 }
 
 function swapi_get_films() {
-	// do we have the films cached?
-	$films = get_transient('swapi_get_films');
-
-	if ($films) {
-		// yep, let's return the cached films
-		return $films;
-	} else {
-		// nope, retrieve the films from the StarWars API
-		$items = swapi_get('films');
-		set_transient('swapi_get_films', $items, 60*60);
-
-		return $items;
-	}
+	return swapi_get('films');
 }
 
 function swapi_get_vehicles() {
-	// do we have the vehicles cached?
-	$vehicles = get_transient('swapi_get_vehicles');
-
-	if ($vehicles) {
-		// yep, let's return the cached vehicles
-		return $vehicles;
-	} else {
-		$items = swapi_get('vehicles');
-		set_transient('swapi_get_vehicles', $items, 60*60);
-
-		return $items;
-	}
+	return swapi_get('vehicles');
 }
 
 function swapi_get_characters() {
-	// do we have the characters cached?
-	$characters = get_transient('swapi_get_characters');
-
-	if ($characters) {
-		// yep, let's return the cached characters
-		return $characters;
-	} else {
-		$items = swapi_get('people');
-		set_transient('swapi_get_characters', $items, 60*60);
-
-		return $items;
-	}
+	return swapi_get('people');
 }
 
 function swapi_get_character($character_id) {
