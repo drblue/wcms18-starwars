f909d896e540e165cef940690941266609e5b471
Johan Nordström <johan@digitalvillage.se>
2019-05-23 15:42:49 +0200

    OMGGGGG-is-it-POSSIBLE good version with abstraction of retrieving multiple pages of items.

--- swapi.php
+++ swapi.php
@@ -12,40 +12,60 @@ function swapi_get_url($url) {
 	return json_decode(wp_remote_retrieve_body($response));
 }
 
-function swapi_get($endpoint, $expiry = 3600) {
+function swapi_get($endpoint, $id = null, $expiry = 3600) {
 	$transient_key = "swapi_get_{$endpoint}";
+	if ($id) {
+		$transient_key .= "_{$id}";
+	}
 	$items = get_transient($transient_key);
 
-	if ($items) {
-		return $items;
+	if (!$items) {
+		if ($id) {
+			$items = swapi_get_url("https://swapi.co/api/{$endpoint}/{$id}");
+		} else {
+			$items = [];
+			$url = "https://swapi.co/api/{$endpoint}/";
+			while ($url) {
+				$data = swapi_get_url($url);
+				if (!$data) {
+					return false;
+				}
+				$items = array_merge($items, $data->results);
 
-	} else {
-		$items = [];
-		$url = "https://swapi.co/api/{$endpoint}/";
-		while ($url) {
-			$data = swapi_get_url($url);
-			if (!$data) {
-				return false;
+				$url = $data->next; // "https://swapi.co/" | null
 			}
-			$items = array_merge($items, $data->results);
-
-			$url = $data->next; // "https://swapi.co/" | null
 		}
 
 		// save for future use
 		set_transient($transient_key, $items, $expiry);
-
-		// return items
-		return $items;
 	}
+
+	// return items
+	return $items;
 }
 
 function swapi_get_films() {
 	return swapi_get('films');
 }
 
+function swapi_get_starships() {
+	return swapi_get('starships');
+}
+
+function swapi_get_planets() {
+	return swapi_get('planets');
+}
+
+function swapi_get_planet($planet_id) {
+	return swapi_get('planets', $planet_id);
+}
+
 function swapi_get_vehicles() {
 	return swapi_get('vehicles');
+}
+
+function swapi_get_vehicle($vehicle_id) {
+	return swapi_get('vehicles', $vehicle_id);
 }
 
 function swapi_get_characters() {
@@ -53,21 +73,5 @@ function swapi_get_characters() {
 }
 
 function swapi_get_character($character_id) {
-	// do we have the films cached?
-	$character = get_transient('swapi_get_character_' . $character_id);
-
-	if ($character) {
-		// yep, let's return the cached character
-		return $character;
-	} else {
-		// nope, retrieve the character from the StarWars API
-		$data = swapi_get_url('https://swapi.co/api/people/' . $character_id);
-		if (!$data) {
-			return false;
-		}
-		set_transient('swapi_get_character_' . $character_id, $data, 60*60*24*7);
-
-		return $data;
-	}
+	return swapi_get('people', $character_id);
 }
-
